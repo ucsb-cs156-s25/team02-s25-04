ad55d9d1ec63e930d73d876b82619688d3ee7f39
Asuasuasu1 <montgomery@ucsb.edu>
2025-04-30 07:30:39 -0700

    commit fixtures

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,34 @@
+const restaurantFixtures = {
+    oneUcsbOrganization: [
+        {
+            "orgCode": "ZPR",
+            "orgTranslationShort": "ZETA PHI RHO",
+            "orgTranslation": "ZETA PHI RHO",
+            "inactive": false
+        },
+    ],
+  
+    threeUcsbOrganizations: [
+        {
+            "orgCode": "ZPR",
+            "orgTranslationShort": "ZETA PHI RHO",
+            "orgTranslation": "ZETA PHI RHO",
+            "inactive": false
+        },
+        {
+            "orgCode": "SKY",
+            "orgTranslationShort": "SKYDIVING CLUB",
+            "orgTranslation": "SKYDIVING CLUB AT UCSB",
+            "inactive": false
+        },
+        {
+            "orgCode": "OSLI",
+            "orgTranslationShort": "STUDENT LIFE",
+            "orgTranslation": "OFFICE OF STUDENT LIFE",
+            "inactive": false
+        }
+        ]
+  };
+  
+  export { restaurantFixtures };
+  
