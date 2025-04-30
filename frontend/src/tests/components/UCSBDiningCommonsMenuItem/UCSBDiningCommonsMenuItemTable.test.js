ae9db8bf5fb7c9273be4470da55f4d7066a1d8db
Ethan Solomon <ethansolomon@ucsb.edu>
2025-04-29 23:03:11 -0700

    fixed tests ?

--- frontend/src/tests/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemTable.test.js
+++ frontend/src/tests/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemTable.test.js
@@ -162,7 +162,8 @@ describe("UCSBDiningCommonsMenuItemTable tests", () => {
         "/api/ucsbdiningcommonsmenuitem",
       );
       expect(axiosMock.history.delete[0].params).toEqual({ id: 1 });
-      expect(toast).toHaveBeenCalledWith("Item deleted");
+      expect(toast).toHaveBeenCalledWith(expect.objectContaining({ message: "Item deleted" }));
+
     });
   });
 });
