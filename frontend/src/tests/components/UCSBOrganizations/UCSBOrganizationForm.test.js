1b980e114126966ac161a730e5aae4c98045f83e
Asuasuasu1 <montgomery@ucsb.edu>
2025-04-30 09:34:56 -0700

    added more test

--- frontend/src/tests/components/UCSBOrganizations/UCSBOrganizationForm.test.js
+++ frontend/src/tests/components/UCSBOrganizations/UCSBOrganizationForm.test.js
@@ -60,6 +60,9 @@ describe("UCSBOrganizationForm tests", () => {
 
     expect(await screen.findByTestId(`${testId}-orgCode`)).toBeInTheDocument();
     expect(screen.getByText(`Org Code`)).toBeInTheDocument();
+
+    expect(await screen.findByTestId(`${testId}-inactive`)).toBeInTheDocument();
+    expect(screen.getByText(`Inactive`)).toBeInTheDocument();
   });
 
   test("that navigate(-1) is called when Cancel is clicked", async () => {
