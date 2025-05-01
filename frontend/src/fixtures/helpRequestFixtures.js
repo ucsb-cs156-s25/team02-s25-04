b94040965a049272a045584358bf7cc002036ceb
Fahim <zfahim540@gmail.com>
2025-04-30 18:43:17 -0700

    Fz - created fixtures for tests

---
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -0,0 +1,43 @@
+const helpRequestFixtures = {
+  oneHelpRequest: {
+    id: 2,
+    requesterEmail: "fahimzaman@ucsb.edu",
+    teamId: "s25-6pm-4",
+    tableOrBreakoutRoom: "4",
+    requestTime: "2025-04-30T18:27:59.04-07:00",
+    explanation: "I have issues creating controller to Update a HelpRequest",
+    solved: false,
+  },
+  threeHelpRequests: [
+    {
+      id: 1,
+      requesterEmail: "fahimzaman@ucsb.edu",
+      teamId: "s25-6pm-4",
+      tableOrBreakoutRoom: "4",
+      requestTime: "2025-04-29T23:26:00-07:00",
+      explanation: "I had issues setting up controller to Post a HelpRequest",
+      solved: false,
+    },
+
+    {
+      id: 3,
+      requesterEmail: "fahimzaman@ucsb.edu",
+      teamId: "s25-6pm-4",
+      tableOrBreakoutRoom: "4",
+      requestTime: "2025-04-29T23:26:00-07:00",
+      explanation: "I can't deploy my website to dokku",
+      solved: true,
+    },
+    {
+      id: 4,
+      requesterEmail: "fahimzaman@ucsb.edu",
+      teamId: "s25-6pm-4",
+      tableOrBreakoutRoom: "4",
+      requestTime: "2025-04-29T23:29:00-07:00",
+      explanation: "I can't figure out how to delete a help request post",
+      solved: true,
+    },
+  ],
+};
+
+export { helpRequestFixtures };
