3513d9e25fd3a928587c991c6fc9150524d2251a
Fahim <zfahim540@gmail.com>
2025-05-02 12:53:12 -0700

    Added Form components, might need to work on tests

---
+++ frontend/src/tests/components/HelpRequest/HelpRequest.test.js
@@ -0,0 +1,109 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import HelpRequestForm from "main/components/HelpRequests/HelpRequestForm";
+import { helpRequestFixtures } from "fixtures/helpRequestFixtures";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const mockedNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedNavigate,
+}));
+
+describe("HelpRequestForm tests", () => {
+  const queryClient = new QueryClient();
+
+  const expectedHeaders = ["Email", "Team ID", "Table/Breakout Room #", "Request Time (in UTC)","Explanation","Is it Solved?"];
+  const testId = "HelpRequestForm";
+
+  test("renders correctly with no initialContents", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <HelpRequestForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
+  });
+
+  test("renders correctly when passing in initialContents", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <HelpRequestForm initialContents={helpRequestFixtures.oneHelpRequest} />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
+
+    expect(await screen.findByTestId(`${testId}-id`)).toBeInTheDocument();
+    expect(screen.getByText(`Id`)).toBeInTheDocument();
+  });
+
+  test("that navigate(-1) is called when Cancel is clicked", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <HelpRequestForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+    expect(await screen.findByTestId(`${testId}-cancel`)).toBeInTheDocument();
+    const cancelButton = screen.getByTestId(`${testId}-cancel`);
+
+    fireEvent.click(cancelButton);
+
+    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
+  });
+
+  test("that the correct validations are performed", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <HelpRequestForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+    const submitButton = screen.getByText(/Create/);
+    fireEvent.click(submitButton);
+
+    await screen.findByText(/Email is required./);
+    expect(screen.getByText(/Team ID is required./)).toBeInTheDocument();
+    expect(screen.getByText(/Table or Breakout Room number is required./)).toBeInTheDocument();
+    expect(screen.getByText(/Request Time is required./)).toBeInTheDocument();
+    expect(screen.getByText(/Explanation is required./)).toBeInTheDocument();
+
+    const requesterEmailInput = screen.getByTestId(`${testId}-requesterEmail`);
+    fireEvent.change(requesterEmailInput, { target: { value: "a".repeat(256) } });
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(screen.getByText(/Max length 255 characters/)).toBeInTheDocument();
+    });
+
+    const explanationInput = screen.getByTestId(`${testId}-explanation`);
+    fireEvent.change(explanationInput, { target: { value: "a".repeat(256) } });
+    fireEvent.click(submitButton);
+    await waitFor(() => {
+        expect(screen.getByText(/Max length 255 characters/)).toBeInTheDocument();
+      });
+  });
+});
