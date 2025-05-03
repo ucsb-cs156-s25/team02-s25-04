62d5eccc3a3cce3ff95f0c577cf5da522ff4479f
Fahim <zfahim540@gmail.com>
2025-05-02 21:50:38 -0700

    how many minor issues can one man overlook

--- frontend/src/tests/pages/HelpRequests/HelpRequestsCreatePage.test.js
+++ frontend/src/tests/pages/HelpRequests/HelpRequestsCreatePage.test.js
@@ -7,7 +7,6 @@ import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-import HelpRequestsCreatePage from "main/pages/HelpRequests/HelpRequestsCreatePage";
 
 describe("HelpRequestsCreatePage tests", () => {
   const axiosMock = new AxiosMockAdapter(axios);
